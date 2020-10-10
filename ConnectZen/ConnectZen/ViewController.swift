b2288889fcc29633ae4f26b42f06b6eba23264b5
Mamta Shah <mamtashah1312@gmail.com>
2020-10-09 23:06:45 -0700

    Initializing ConnectZen

---
+++ ConnectZen/ConnectZen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConnectZen
+//
+//  Created by Mamta Shah on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello World")
+    }
+
+
+}
+
