028ed5eb98b2e59e8c40e5f1cc67c7537fd0eb41
Mamta Shah <mamtashah1312@gmail.com>
2020-11-16 13:37:24 -0800

    Backend for Day and Time preference

---
+++ ConnectZen/ConnectZen/Constants/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  ConnectZen
+//
+//  Created by Mamta Shah on 11/16/20.
+//
+
+import Foundation
+
+let Days:[String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
